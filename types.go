ed4a05d711e62f54232cfa6a0038353fb2490d9d
shankeerthan-kasilingam <shankeerthan1995@gmail.com>
2022-04-29 07:31:20 +0530

    Add types
    Signed-off-by: shankeerthan-kasilingam <shankeerthan1995@gmail.com>

---
+++ types.go
@@ -0,0 +1,83 @@
+package mathif
+
+type Op string
+
+type CondOp Op
+
+const (
+	OpEq   = CondOp("eq")
+	OpNEq  = CondOp("neq")
+	OpLt   = CondOp("lt")
+	OpGt   = CondOp("gt")
+	OpLtEq = CondOp("lteq")
+	OpGtEq = CondOp("gteq")
+)
+
+type MathOp Op
+
+const (
+	OpAdd        = MathOp("add")
+	OpMinus      = MathOp("sub")
+	OpMultiply   = MathOp("mul")
+	OpDivide     = MathOp("div")
+	OpPercentage = MathOp("perc")
+)
+
+type CondConjOp Op
+
+const (
+	OpAnd = CondConjOp("and")
+	OpOr  = CondConjOp("or")
+	OpNot = CondConjOp("not")
+)
+
+type ConditionSpec struct {
+	op Op
+	v1 interface{}
+	v2 interface{}
+}
+
+// Todo : return true if the
+func (cs *ConditionSpec) Verify() bool {
+
+}
+
+func (cs *ConditionSpec) Eval() bool {
+	// Todo check types
+	// if float
+	// if string
+	// if int
+	// bool
+	// if ConditionSpec ev
+}
+
+// Todo : comment
+// It will contain
+type ConditionGroupSpec struct {
+	cond *ConditionSpec
+	parent   *ConditionGroupSpec
+	vars     []*VariableSpec
+	exps     []*ExpresionSpec
+	subConds []*ConditionGroupSpec
+}
+
+type ExpresionSpec struct {
+	opCode MathOp
+	v1 VariableSpec
+	v2 VariableSpec
+}
+
+type VarType string
+
+const (
+	Const  = VarType("constant")
+	ExpOut = VarType("expout")
+	Ref    = VarType("ref")
+)
+
+type VariableSpec struct {
+	VarType VarType
+	name    string
+	symbol  string
+	value   interface{}
+}
