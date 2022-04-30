0707c62695c072e3c6af1acc770226c49fe7ef77
shankeerthan-kasilingam <shankeerthan1995@gmail.com>
2022-04-30 09:25:02 +0530

    Add load json calculation schema
    Signed-off-by: shankeerthan-kasilingam <shankeerthan1995@gmail.com>

--- mathexp.go
+++ mathexp.go
@@ -1,1 +1,15 @@
 package mathexp
+
+import "encoding/json"
+
+type MathExp struct {
+	ExpWrapper *ConditionGroupSpec
+}
+
+func New(expJson []byte) (*MathExp, error) {
+	var rootCondGrpSpec ConditionGroupSpec
+	if err := json.Unmarshal(expJson, &rootCondGrpSpec); err != nil {
+		return nil, err
+	}
+	return &MathExp{ExpWrapper: &rootCondGrpSpec}, nil
+}
