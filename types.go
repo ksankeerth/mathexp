8b9c5b59fe1ba1e56a827da6ca0eb79a789924e4
shankeerthan-kasilingam <shankeerthan1995@gmail.com>
2022-04-30 09:23:55 +0530

    Refractor types for simplicity
    Signed-off-by: shankeerthan-kasilingam <shankeerthan1995@gmail.com>

--- types.go
+++ types.go
@@ -1,85 +1,29 @@
 package mathexp
 
-type Op string
-
-type CondOp Op
-
-const (
-	OpEq   = CondOp("eq")
-	OpNEq  = CondOp("neq")
-	OpLt   = CondOp("lt")
-	OpGt   = CondOp("gt")
-	OpLtEq = CondOp("lteq")
-	OpGtEq = CondOp("gteq")
-)
-
-type MathOp Op
-
-const (
-	OpAdd        = MathOp("add")
-	OpMinus      = MathOp("sub")
-	OpMultiply   = MathOp("mul")
-	OpDivide     = MathOp("div")
-	OpPercentage = MathOp("perc")
-)
-
-type CondConjOp Op
-
-const (
-	OpAnd = CondConjOp("and")
-	OpOr  = CondConjOp("or")
-	OpNot = CondConjOp("not")
-)
-
 type ConditionSpec struct {
-	op Op
-	v1 interface{}
-	v2 interface{}
+	Op string
+	V1 interface{}
+	V2 interface{}
 }
 
-// Todo : return true if the
-func (cs *ConditionSpec) Verify() bool {
-
-}
-
-func (cs *ConditionSpec) Eval() bool {
-	// Todo check types
-	// if float
-	// if string
-	// if int
-	// bool
-	// if ConditionSpec ev
-}
-
-// Todo : comment
-// It will contain
-type ConditionGroupSpec struct {
-	cond     *ConditionSpec
-	parent   *ConditionGroupSpec
-	vars     []*VariableSpec
-	exps     []*ExpresionSpec
-	subConds []*ConditionGroupSpec
+type VarSpec struct {
+	Type  string
+	Name  string
+	Sym   string
+	Value interface{}
+	// TODO : add unit information
 }
 
 type ExpresionSpec struct {
-	opCode MathOp
-	v1     VariableSpec
-	v2     VariableSpec
+	Op  string
+	V1  string
+	V2  string
+	Out string
 }
 
-type VarType string
-
-const (
-	Const  = VarType("const")
-	ExpOut = VarType("expout")
-	Ref    = VarType("ref")
-	In     = VarType("in")
-)
-
-type VariableSpec struct {
-	VarType VarType
-	name    string
-	symbol  string
-	value   interface{}
-	// TODO : add unit information
+type ConditionGroupSpec struct {
+	Cond               *ConditionSpec
+	Vars               []*VarSpec
+	Expressions        []*ExpresionSpec
+	SubConditionGroups []*ConditionGroupSpec
 }
