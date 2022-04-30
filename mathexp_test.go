ff7f2eb1ac34ca7c329c19eef739d2b0270f16c4
shankeerthan-kasilingam <shankeerthan1995@gmail.com>
2022-04-30 10:10:57 +0530

    Refractor loadjson test
    Signed-off-by: shankeerthan-kasilingam <shankeerthan1995@gmail.com>

--- mathexp_test.go
+++ mathexp_test.go
@@ -1,9 +1,9 @@
 package mathexp_test
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/shankeerthan-kasilingam/mathexp"
@@ -11,24 +11,40 @@ import (
 
 func TestMathExpFromJson(t *testing.T) {
 
-	testExpElecCalJson, err := os.Open("./test-data/electricity-calc-sample.json")
-	defer func() {
-		if err := testExpElecCalJson.Close(); err != nil {
-			t.Fatalf("Failed to close the file %v", err)
-		}
-	}()
-	if err != nil {
-		t.Fatalf("Failed to open test data %v", err)
-	}
+	byteData, file := openFile(t, "./test-data/electricity-calc-sample.json")
+	defer closeFile(t, file)
 
-	byteData, err := ioutil.ReadAll(testExpElecCalJson)
-	if err != nil {
-		t.Fatalf("Failed to read test data %v", err)
-	}
 	mathExp, err := mathexp.New(byteData)
 	if err != nil {
 		t.Fatalf("Failed to parse the data %v", err)
 	}
-	fmt.Println(mathExp.ExpWrapper)
+	if len(mathExp.ExpWrapper.SubConditionGroups) != 3 {
+		t.Errorf("SubConditionsGroups are not correct, got %d want %d", len(mathExp.ExpWrapper.SubConditionGroups), 3)
+	}
+	if len(mathExp.ExpWrapper.Expressions) != 0 {
+		t.Errorf("Expressions are not empty, got %d want %d", len(mathExp.ExpWrapper.Expressions), 0)
+	}
+	if strings.Compare(mathExp.ExpWrapper.SubConditionGroups[1].Cond.Op, "and") != 0 {
+		t.Errorf("Operation doesn't match got %s want %s", mathExp.ExpWrapper.SubConditionGroups[1].Cond.Op, "and")
+	}
 
+}
+
+func openFile(t *testing.T, filePath string) ([]byte, *os.File) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		t.Fatalf("Failed to open file %s : %v", filePath, err)
+	}
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.Fatalf("Failed to read file %s : %v", filePath, err)
+	}
+	return data, file
+}
+
+func closeFile(t *testing.T, file *os.File) {
+	err := file.Close()
+	if err != nil {
+		t.Fatalf("Failed to close file %s : %v", file.Name(), err)
+	}
 }
